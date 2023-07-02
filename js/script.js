de1dbf28f0d159066baa14ac0ac380fbd61fc0ac
Arturo Mora-Rioja <arturomora@gmail.com>
2023-07-02 19:50:26 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+document.querySelector('#frmTemperature').addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const temperature = parseFloat(document.querySelector('#txtTemperature').value);
+    const from = document.querySelector('#lstFrom').value;
+    const to = document.querySelector('#lstTo').value;
+    let conversion = '';
+    
+    switch (from + to) {
+        case 'CF': conversion = (temperature * 1.8) + 32;           break;
+        case 'CK': conversion = temperature + 273.15;               break;
+        case 'FC': conversion = (temperature - 32) / 1.8;           break;
+        case 'FK': conversion = (temperature + 459.67) * (5 / 9);   break;
+        case 'KC': conversion = temperature - 273.15;               break;
+        case 'KF': conversion = (temperature * (5 / 9)) - 459.67;   break;
+    }
+
+    if (conversion === '') {
+        document.querySelector('#fromText').innerHTML = '';
+        document.querySelector('#toText').innerHTML = '';
+    } else {
+        document.querySelector('#fromText').innerHTML = temperature + '&deg;' + from + ' = ';
+        document.querySelector('#toText').innerHTML = conversion.toFixed(2) + '&deg;' + to;
+        document.querySelector('#results').classList.add('show');
+    }    
+});
